1ac728b0b86e6a0ec0444ee7c1e3e5268aa85756
Vipoup <60632610+Vipoup@users.noreply.github.com>
2022-03-25 20:06:40 +0000

    Update CodeWithBadPractices.js

--- CodeWithBadPractices.js
+++ CodeWithBadPractices.js
@@ -16,3 +16,4 @@ console.log("array");
 console.log("array(");
 
 console.log("object");
+console.log("super long text that exceeds 80 characters and needs to be trimmed down to under 80 coulmns for code smellihng purposes);
